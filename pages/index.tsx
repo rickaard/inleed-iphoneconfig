17134902a001bb23b1f278a26615bb19eac13795
Rickard jobblaptop <rickaard@gmail.com>
2020-11-24 17:03:16 +0100

    fu iphone

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import download from 'downloadjs';
+// import download from 'downloadjs';
 
 // import Link from 'next/link'
 import Layout from '../components/Layout';
