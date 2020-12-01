bf889a64300dd5b213819d1a74946c6739c1fb0f
Rickard jobblaptop <rickaard@gmail.com>
2020-12-01 17:02:39 +0100

    styling

--- data.ts
+++ data.ts
@@ -10,7 +10,7 @@ export const texts: IInstruction[] = [
     {
         id: 1,
         textHeader: 'Fyll i din e-postadress',
-        textPara: 'Börja med att fylla i din e-postadres. Tryck sedan på HÄMTA. Notera att din domän måste peka mot oss för att kunna använda denna tjänst',
+        textPara: 'Börja med att fylla i din e-postadres. Tryck sedan på HÄMTA. Notera att din domän måste peka mot oss för att kunna använda denna tjänst.',
         imgUrl: 'pics/1.png',
         imgAlt: 'Fyll i din e-postadress'
     },
@@ -24,14 +24,14 @@ export const texts: IInstruction[] = [
     {
         id: 3,
         textHeader: 'Profil hämtad i inställningar',
-        textPara: 'När profilen nu är hämtad behöver du gå in i inställningarna. Under ditt namn ska du kunna se att det finns en ny Profil hämtad. Gå in på denna',
+        textPara: 'När profilen nu är hämtad behöver du gå in i inställningarna. Under ditt namn ska du kunna se att det finns en ny Profil hämtad. Gå in på denna.',
         imgUrl: 'pics/4.png',
         imgAlt: 'Gå in på profilen via inställningar'
     },
     {
         id: 4,
         textHeader: 'Installera profil',
-        textPara: 'Sista steget är att installera din nya profil. Tryck högst upp till höger på Installera och skriv sedan in ditt lösenord till e-posten. Därefter är allt klart och du kan använda din e-posten i din iPhone',
+        textPara: 'Sista steget är att installera din nya profil. Tryck högst upp till höger på Installera och skriv sedan in ditt lösenord till e-posten. Därefter är allt klart och du kan använda din e-posten i din iPhone.',
         imgUrl: 'pics/5.png',
         imgAlt: 'Installera profil'
     },
